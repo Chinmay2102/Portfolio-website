1ab95e3f65f5eb47797060bd711e6517acf553bf
Chinmay Gawande <gawandechinu@gmail.com>
2026-01-13 17:56:22 +0530

    Day 9: Add POST API for creating projects

--- server/projects/views.py
+++ server/projects/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Project
 from .serializers import ProjectSerializer
 
 # Create your views here.
-@api_view(["GET"])
+@api_view(["GET","POST"])
 def project_list(request):
-    projects = Project.objects.all()
-    serializer = ProjectSerializer(projects,many=True)
-    return Response(serializer.data)
+    if request.method =='GET':
+        projects = Project.objects.all()
+        serializer = ProjectSerializer(projects,many=True)
+        return Response(serializer.data)
+    
+    if request.method =='POST':
+        serializer = ProjectSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        print(serializer.errors)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
